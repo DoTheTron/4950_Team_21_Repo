5530aa1705b42e4adf903aea45f626586983112d
Uchenna Wosu <uchennawosu@gmail.com>
2022-10-05 18:24:43 -0400

    Completed Image Save function

--- lab1/cam_interfacing.m
+++ lab1/cam_interfacing.m
@@ -1,26 +1,35 @@
-%% ECE 4950 Fall 2020 Project 2 Demo - Camera Setup
-close all
-clc
-clear
-clear('cam')
+%% Definitions and Constants
+bg_filename = 'bg_img.png';
+USB_CAM_NAME = 'USB2.0 PC CAMERA';  %name of USB camera
 
-%% Check Camera List (may have to install MATLAB package)
-cam_list = webcamlist
-%% Assign webcam to be used
-% Pick an index from the camera list above
-cam_name = cam_list{3}
-%% Check webcam properties
-cam = webcam(cam_name)
-%% Preview cam
-%preview(cam)
-%% Snapshot 1
+%% ECE 4950 Fall 2020 Project 2 Demo - Camera Setup
+prog_init();
+cam = init_cam(webcamlist,USB_CAM_NAME);
+
+%% Capturing and Saving BG Image
 img = snapshot(cam);
 figure();
-imshow(img)
-title('Snapshot 1')
+imshow(img);
+title('Background Image Original');
+
 %% Binary Image processing
-[bg_img_fname, UC] = imsave(img)
+imwrite(img, bg_filename); %saves image to filename provided
+
 %Image_Orig = imread(img);
 %Image_Background = imread('NoiseBackground.png');
 %[height,width,depth] = size(Image_Orig);
 %[height_b,width_b,depth_b] = size(Image_Background
+
+%% Init Program
+function prog_init()
+    close all;
+    clc;
+    clear;
+    clear('cam');
+end
+%% Camera Init Function
+function cam_profile = init_cam(webcam_list,cam_name)
+    %searches webcam list camera name provided, then returns camera struct
+    cam_profile = webcam(find(contains(webcam_list,cam_name)));
+    cam_profile.Brightness = 50; %scale brightness of image (idk what max val is -Uche)
+end
