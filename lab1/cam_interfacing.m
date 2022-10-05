5a66bcce9ee586a97d681a2d2156e6a8ed6b6630
Uchenna Wosu <uchennawosu@gmail.com>
2022-10-05 12:02:03 -0400

    camera testing

--- lab1/cam_interfacing.m
+++ lab1/cam_interfacing.m
@@ -3,3 +3,24 @@ close all
 clc
 clear
 clear('cam')
+
+%% Check Camera List (may have to install MATLAB package)
+cam_list = webcamlist
+%% Assign webcam to be used
+% Pick an index from the camera list above
+cam_name = cam_list{3}
+%% Check webcam properties
+cam = webcam(cam_name)
+%% Preview cam
+%preview(cam)
+%% Snapshot 1
+img = snapshot(cam);
+figure();
+imshow(img)
+title('Snapshot 1')
+%% Binary Image processing
+[bg_img_fname, UC] = imsave(img)
+%Image_Orig = imread(img);
+%Image_Background = imread('NoiseBackground.png');
+%[height,width,depth] = size(Image_Orig);
+%[height_b,width_b,depth_b] = size(Image_Background
