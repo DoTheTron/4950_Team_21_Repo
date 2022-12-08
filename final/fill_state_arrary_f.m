42e4dcc86db74c4c2ae4801dedfbd1c41be44bfb
Uchenna Wosu <uchennawosu@gmail.com>
2022-12-07 23:55:00 -0500

    hour*

--- final/fill_state_arrary_f.m
+++ final/fill_state_arrary_f.m
@@ -3,22 +3,22 @@
     completes coolor entries and valid angle positions for washers
 %}
 function complete_state = fill_state_arrary_f(gameState)
-    valid_angle_thresh  = [40,90,140,180,210,270,300,360]; %reference to check angle's position
+    valid_angle_thresh  = [0,40,90,140,180,210,270,300,360]; %reference to check angle's position
     valid_angle         = [40,90,140,180,210,270,300,360]; %actual angular location of gameboard position
-    [entries,~]         = size(valid_angle_thresh);
+    [~,entries]         = size(valid_angle_thresh);
     state_creation      = struct('color','blank','Angle',0,'location',{});
 
-    for sector_angle = 1:entries
+    for sector_angle = 2:entries
         for i = 1:gameState(1).Num_of_Shapes
-            if (gameState(i).Angle < sector_angle)
-                state_creation(sector_angle).color = gameState(i).Color;
+            if ((gameState(i).Angle >= valid_angle_thresh(sector_angle-1)) && (gameState(i).Angle < valid_angle_thresh(sector_angle)))
+                state_creation(sector_angle).color = gameState(i).color;
                 state_creation(sector_angle).location = gameState(i).location;
             else
                 state_creation(sector_angle).color = 'empty';
                 state_creation(sector_angle).location = [0,0];
             end
         end
-        state_creation(sector_angle).Angle = valid_angle(sector_angle);
+        state_creation(sector_angle-1).Angle = valid_angle(sector_angle-1);
     end
     complete_state = state_creation;
 end
