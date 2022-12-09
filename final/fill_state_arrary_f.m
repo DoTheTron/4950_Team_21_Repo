f9e76932198c169645381bf46d1f5e4b82c6829f
Uchenna Wosu <uchennawosu@gmail.com>
2022-12-09 11:30:57 -0500

    fixed full state struct for algorithim

--- final/fill_state_arrary_f.m
+++ final/fill_state_arrary_f.m
@@ -3,7 +3,7 @@
     completes coolor entries and valid angle positions for washers
 %}
 function complete_state = fill_state_arrary_f(gameState)
-    valid_angle_thresh  = [0,40,90,140,180,210,270,300,360]; %reference to check angle's position
+    valid_angle_thresh  = [0,50,90,140,190,220,270,320,360]; %reference to check angle's position
     valid_angle         = [0,40,-45,-90,-135,-180,-225,-270]; %actual angular location of gameboard position
     [~,entries]         = size(valid_angle_thresh);
     state_creation      = struct('color','blank','Angle',0,'location',{});
@@ -13,6 +13,7 @@ function complete_state = fill_state_arrary_f(gameState)
             if ((gameState(i).Angle >= valid_angle_thresh(sector_angle-1)) && (gameState(i).Angle < valid_angle_thresh(sector_angle)))
                 state_creation(sector_angle-1).color = gameState(i).color;
                 state_creation(sector_angle-1).location = gameState(i).location;
+                break;
             else
                 state_creation(sector_angle-1).color = 'empty';
                 state_creation(sector_angle-1).location = [0,0];
